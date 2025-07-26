52c6b13d774806f9fbeb14c907f64de86b21ab62
Francisco Gutiérrez <48999008+KiskoGutierrez@users.noreply.github.com>
2025-07-27 01:01:56 +0200

    Update ProductoFactory.php

--- full-project/tienda-online-api/database/factories/ProductoFactory.php
+++ full-project/tienda-online-api/database/factories/ProductoFactory.php
@@ -12,10 +12,10 @@ class ProductoFactory extends Factory
     public function definition()
     {
         return [
-            'nombre' => $this->faker->words(2, true),
-            'precio' => $this->faker->randomFloat(2, 5, 500),
-            'stock' => $this->faker->numberBetween(1, 100),
-            'imagen' => null // o puedes usar $this->faker->imageUrl() si usas URLs reales
+            'nombre' => $this->faker->words(2, true),        // Nombre ficticio
+            'precio' => $this->faker->randomFloat(2, 5, 500), // Precio entre 5 y 500 €
+            'stock' => $this->faker->numberBetween(1, 100),   // Stock entre 1 y 100 unidades
+            'imagen' => null // o $this->faker->imageUrl() si se desea usar una URL
         ];
     }
 }
