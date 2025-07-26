0afcb486e2ea6810307307ab70c281283f31adb4
Francisco Gutiérrez <48999008+KiskoGutierrez@users.noreply.github.com>
2025-07-27 00:54:03 +0200

    Update Order.php

--- full-project/tienda-online-api/app/Models/Order.php
+++ full-project/tienda-online-api/app/Models/Order.php
@@ -6,8 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
+    // Permite asignación masiva de user_id y total
     protected $fillable = ['user_id', 'total'];
 
+    // Relación: una orden tiene muchos items
     public function items()
     {
         return $this->hasMany(OrderItem::class);
